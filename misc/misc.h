d81abfe1e77ed34a60c108c7a43c668a27d50c2f
Bas Visscher <bas.visscher24@gmail.com>
2021-01-17 16:44:34 +0100

    Added callback as component

---
+++ misc/misc.h
@@ -0,0 +1,15 @@
+/*
+ * misc.h
+ *
+ *  Created on: Jan 17, 2021
+ *      Author: Bas
+ */
+
+#ifndef COMPONENTS_MISC_MISC_H_
+#define COMPONENTS_MISC_MISC_H_
+
+#include "callback.h"
+
+
+
+#endif /* COMPONENTS_MISC_MISC_H_ */
